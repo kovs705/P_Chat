34b7c56c4093ce4ea5ad9b1bf10a0e6356afe395
Eugene Kovs <smartkovsik@gmail.com>
2023-11-16 00:34:46 +0300

    Initial Commit

---
+++ P_Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P_Chat
+//
+//  Created by Eugene Kovs on 16.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
